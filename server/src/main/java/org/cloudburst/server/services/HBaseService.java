c1b55d95cdc15f78c908217dcf82edf56e989e87
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-10-18 15:41:12 -0700

    Added HBase Service

---
+++ server/src/main/java/org/cloudburst/server/services/HBaseService.java
@@ -0,0 +1,48 @@
+package org.cloudburst.server.services;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class HBaseService {
+    private final static Logger logger = LoggerFactory.getLogger(HBaseService.class);
+
+    private HTable table;
+    private String tableName;
+    private String columnFamily;
+    private String qualifier;
+
+    public HBaseService(String tableName, String columnFamily, String qualifier) {
+        Configuration config = HBaseConfiguration.create();
+        try {
+            this.table = new HTable(config, tableName);
+        } catch (IOException e) {
+            logger.error("IO Exception while trying to connect to HBase table");
+            e.printStackTrace();
+        }
+        this.tableName = tableName;
+        this.columnFamily = columnFamily;
+        this.qualifier = qualifier;
+    }
+
+    public String getRecord(String rowKey) {
+        Get get = new Get(Bytes.toBytes(rowKey));
+        Result result = null;
+        try {
+            result = table.get(get);
+        } catch (IOException e) {
+            logger.error("IO Exception when trying to read from HBase table");
+            e.printStackTrace();
+        }
+        byte[] value = result.getValue(Bytes.toBytes(columnFamily),Bytes.toBytes(qualifier));
+
+        return Bytes.toString(value);
+    }
+}
