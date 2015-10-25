acf8bc2c5369c5f5a6247d65c6dcbe1ac065ea2d
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-10-25 01:19:48 -0700

    Completed implementation of HBase service for loading data

--- etl/src/main/java/org/cloudburst/etl/util/TextCensor.java
+++ etl/src/main/java/org/cloudburst/etl/util/TextCensor.java
@@ -6,9 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.cloudburst.etl.model.Tweet;
