bafc0452993ae323aefdac76c93e161369c34c82
Federico Ponte <fedep3@gmail.com>
2015-10-25 22:59:25 -0700

    sql optimizatinos

--- etl/src/main/resources/table.sql
+++ etl/src/main/resources/table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tweet (
     tweetId BIGINT primary key,
     usedId BIGINT,
     creationTime TIMESTAMP not null,
-    text varchar(256) not null,
+    text char(192) not null,
     score int not null default 0
 ) ENGINE = MyISAM;
 
