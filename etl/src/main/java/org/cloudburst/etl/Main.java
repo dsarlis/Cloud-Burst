10af3953a5a048a206f1ae34e9d13daa450726e0
Federico Ponte <fedep3@gmail.com>
2015-10-27 15:13:39 -0700

    Serializing set class

--- etl/src/main/java/org/cloudburst/etl/Main.java
+++ etl/src/main/java/org/cloudburst/etl/Main.java
@@ -1,10 +1,9 @@
 package org.cloudburst.etl;
 
-import java.io.IOException;
+import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,6 +21,7 @@ public class Main {
 	private final static Logger logger = LoggerFactory.getLogger(Main.class);
 	private final static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_NUMBER);
 	private final static AtomicInteger counter = new AtomicInteger(1);
+	private static final String QUEUE_FILENAME = "queue.object";
 
 	public static void main(String[] args) throws InterruptedException, SQLException {
 		LoggingConfigurator.configureFor(LoggingConfigurator.Environment.PRODUCTION);
@@ -31,7 +31,39 @@ public class Main {
 		if (args.length == 2) {
 			tweetFileNames = tweetFileNames.subList(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 		}
+		initStructures();
 
+		Set<Long> uniqueTweetIds = getTweetIdsSet();
+
+		for (String tweetFileName : tweetFileNames) {
+			MySQLService mySQLService = new MySQLService(new MySQLConnectionFactory());
+			Worker worker = new Worker(tweetFileName, tweetsDataStoreService, mySQLService, uniqueTweetIds, counter);
+
+			threadPool.execute(worker);
+		}
+
+		while (counter.get() < tweetFileNames.size()) {
+			logger.info("{} threads done", counter.get());
+			Thread.sleep(TWO_MINUTES);
+		}
+
+		writeTweetIdsSet(uniqueTweetIds);
+		MySQLConnectionFactory.shutdown();
+	}
+
+	private static void writeTweetIdsSet(Set<Long> uniqueTweetIds) {
+		try (FileOutputStream classFile = new FileOutputStream(QUEUE_FILENAME);
+			 ObjectOutputStream objectOutputStream = new ObjectOutputStream(classFile)) {
+
+			objectOutputStream.writeObject(uniqueTweetIds);
+		} catch (FileNotFoundException ex) {
+			logger.error("File not found");
+		} catch (IOException ex) {
+			logger.error("Problem writing class", ex);
+		}
+	}
+
+	private static void initStructures() {
 		Properties boneCPConfigProperties = new Properties();
 		try {
 			boneCPConfigProperties.load(Main.class.getResourceAsStream("/bonecp.properties"));
@@ -46,22 +78,25 @@ public class Main {
 		} catch (IOException ex) {
 			logger.error("Problem reading text-processing files!", ex);
 		}
+	}
 
-		Set<Long> uniqueTweetIds = Collections.newSetFromMap(new ConcurrentHashMap<Long, Boolean>());
+	private static Set<Long> getTweetIdsSet() {
+		Set<Long> uniqueTweetIds = null;
 
-		for (String tweetFileName : tweetFileNames) {
-			MySQLService mySQLService = new MySQLService(new MySQLConnectionFactory());
-			Worker worker = new Worker(tweetFileName, tweetsDataStoreService, mySQLService, uniqueTweetIds, counter);
+		try(FileInputStream classFile = new FileInputStream(QUEUE_FILENAME);
+			ObjectInputStream objectInputStream = new ObjectInputStream(classFile)) {
 
-			threadPool.execute(worker);
+			uniqueTweetIds = (Set<Long>) objectInputStream.readObject();
+		} catch (FileNotFoundException ex) {
+			uniqueTweetIds = Collections.newSetFromMap(new ConcurrentHashMap<Long, Boolean>());
+		} catch (IOException ex) {
+			logger.error("Problem reading file", ex);
+			System.exit(-1);
+		} catch (ClassNotFoundException ex) {
+			logger.error("Call problem", ex);
+			System.exit(-1);
 		}
-
-		while (counter.get() < tweetFileNames.size()) {
-			logger.info("{} threads done", counter.get());
-			Thread.sleep(TWO_MINUTES);
-		}
-
-		MySQLConnectionFactory.shutdown();
+		return uniqueTweetIds;
 	}
 
 }
