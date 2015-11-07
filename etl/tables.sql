7b4f2ec29738600c9a147911d39624b71b9e2638
Federico Ponte <fedep3@gmail.com>
2015-11-07 13:46:18 -0800

    Added table and process script

---
+++ etl/tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS tweets (
+    tweetId BIGINT primary key,
+    userId BIGINT not null,
+    creationTime TIMESTAMP not null,
+    followersCount int not null default 0,
+    score int not null default 0,
+    text VARBINARY(1024) not null
+) ENGINE = MyISAM;
+
+CREATE TABLE IF NOT EXISTS hashtags (
+    hashtag VARBINARY(1024) not null,
+    tweetId BIGINT not null,
+    count int not null,
+    PRIMARY KEY (hashtag, tweetId)
+) ENGINE = MyISAM;
+
+
+create index userid_creation_date_index on tweets(userId, creationTime);
+
+
+load data infile 'dump.csv' into table tweets fields terminated by ',' lines terminated by '\n' SET text = UNHEX(text);
