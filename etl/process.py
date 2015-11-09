b703bc6e7c2690e81866a7e90c884b16e9befbe9
Federico Ponte <fedep3@gmail.com>
2015-11-09 09:10:10 -0800

    Minor details

--- etl/process.py
+++ etl/process.py
@@ -3,4 +3,3 @@ from sys import argv
 for line in open(argv[1]):
     tweet_id, hashtags, hashtags_count, user_id, created_at, followers_count, score, text = line.replace('\n', '').split('\t')
     print ','.join([tweet_id, user_id, created_at, followers_count, score, text])
-    
