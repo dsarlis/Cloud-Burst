66535c18b1f1f4bab90fca29b55d86809479a9b1
Gaurav Walia <email.gaurav.walia@gmail.com>
2015-11-10 19:04:45 -0800

    Adding Q4 Servlet in web.xml mapping

--- server/src/main/java/org/cloudburst/server/CloudBurstContext.java
+++ server/src/main/java/org/cloudburst/server/CloudBurstContext.java
@@ -1,17 +1,18 @@
 package org.cloudburst.server;
 
-import org.cloudburst.server.servlets.Q1Servlet;
-import org.cloudburst.server.servlets.Q2Servlet;
-import org.cloudburst.server.util.LoggingConfigurator;
-import org.cloudburst.server.util.MySQLConnectionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.TimeZone;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.cloudburst.server.servlets.Q1Servlet;
+import org.cloudburst.server.servlets.Q2Servlet;
+import org.cloudburst.server.servlets.Q4Servlet;
+import org.cloudburst.server.util.MySQLConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Servlet context where everything is initialized.
@@ -30,8 +31,11 @@ public class CloudBurstContext implements ServletContextListener {
         } catch (IOException ex) {
             logger.error("Problem reading properties", ex);
         }
+
         Q1Servlet.setFirstLine(configProperties.getProperty("team.id"), configProperties.getProperty("team.aws.id"));
         Q2Servlet.setFirstLine(configProperties.getProperty("team.id"), configProperties.getProperty("team.aws.id"));
+        Q4Servlet.setFirstLine(configProperties.getProperty("team.id"), configProperties.getProperty("team.aws.id"));
+
         TimeZone.setDefault(TimeZone.getTimeZone("Etc/GMT+4"));
         logger.info("Server started");
     }
@@ -43,4 +47,3 @@ public class CloudBurstContext implements ServletContextListener {
     }
 
 }
-
