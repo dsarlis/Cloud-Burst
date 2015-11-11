a2d06300cd126672e73177b62e42ef1517715ba3
Gaurav Walia <email.gaurav.walia@gmail.com>
2015-11-10 17:53:39 -0800

    Adding Q4 Servlet, and new methods to fetch top hashtags in MySQLService

--- server/src/main/java/org/cloudburst/server/services/MySQLService.java
+++ server/src/main/java/org/cloudburst/server/services/MySQLService.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 
 import org.cloudburst.server.util.MySQLConnectionFactory;
@@ -18,44 +17,76 @@ import org.slf4j.LoggerFactory;
  */
 public class MySQLService {
 
-	private static final char SPACE = ' ';
-	private static final char PLUS = '+';
-	private static final String COLON = ":";
-	private static final String NEW_LINE = "\n";
-	private static final String SELECT_QUERY = "SELECT * FROM tweets WHERE userId=? AND creationTime=? ORDER BY tweetId";
+    private static final char SPACE = ' ';
+    private static final char PLUS = '+';
+    private static final String COLON = ":";
+    private static final String NEW_LINE = "\n";
+    private static final String QUERY_2 = "SELECT * FROM tweets WHERE userId=? AND creationTime=? ORDER BY tweetId";
+    private static final String QUERY_4 = "select * from hashtags where hashtag=? order by totalHashTagCount desc, createdAtDate asc limit ?;";
 
-	private final static Logger logger = LoggerFactory.getLogger(MySQLService.class);
+    private final static Logger logger = LoggerFactory.getLogger(MySQLService.class);
 
-	private MySQLConnectionFactory factory;
+    private MySQLConnectionFactory factory;
 
-	public MySQLService(MySQLConnectionFactory factory) {
-		this.factory = factory;
-	}
+    public MySQLService(MySQLConnectionFactory factory) {
+        this.factory = factory;
+    }
 
-	/**
-	 * Return formatted result with the (tweet id, score) from a given user and timestamp.
-	 */
-	public String getTweetResult(long userId, String creationTime) {
+    /**
+     * Return formatted result with the (tweet id, score) from a given user and
+     * timestamp.
+     */
+    public String getTweetResult(long userId, String creationTime) {
 
-		StringBuilder builder = new StringBuilder();
-		try (Connection connection = factory.getConnection()) {
-			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
-			preparedStatement.setLong(1, userId);
-			preparedStatement.setTimestamp(2, Timestamp.valueOf(creationTime.replace(PLUS, SPACE)));
+        StringBuilder builder = new StringBuilder();
+        try (Connection connection = factory.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY_2);
+            preparedStatement.setLong(1, userId);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(creationTime.replace(PLUS, SPACE)));
 
-			ResultSet rs = preparedStatement.executeQuery();
+            ResultSet rs = preparedStatement.executeQuery();
 
-			while (rs.next()) {
-				builder.append(rs.getLong("tweetId")).append(COLON);
-				builder.append(rs.getInt("score")).append(COLON);
-				builder.append(TextCensor.censorBannedWords(new
-						String(rs.getBytes("text"), StandardCharsets.UTF_8)).replace(";", "\n"));
-				builder.append(NEW_LINE);
-			}
-		} catch (SQLException ex) {
-			logger.error("Problem executing statement", ex);
-		}
+            while (rs.next()) {
+                builder.append(rs.getLong("tweetId")).append(COLON);
+                builder.append(rs.getInt("score")).append(COLON);
+                builder.append(TextCensor.censorBannedWords(new String(rs.getBytes("text"), StandardCharsets.UTF_8))
+                        .replace(";", "\n"));
+                builder.append(NEW_LINE);
+            }
+        } catch (SQLException ex) {
+            logger.error("Problem executing statement", ex);
+        }
 
-		return builder.toString();
-	}
+        return builder.toString();
+    }
+
+    /**
+     * Returns the top hashtag sorted by hashtagCount and CreatedDate.
+     */
+    public String getTopHashtags(String hashtag, int limit) {
+
+        StringBuilder builder = new StringBuilder();
+        try (Connection connection = factory.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY_4);
+            preparedStatement.setBytes(1, hashtag.getBytes());
+            preparedStatement.setInt(2, limit);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                /* Date:Count:[List of user ids]:SourceTweetText\n */
+                builder.append(rs.getDate("createdAtDate")).append(COLON);
+                builder.append(rs.getInt("totalHashTagCount")).append(COLON);
+                builder.append(String.format("[%s]", rs.getString("sortedUniqueUserList"))).append(COLON);
+                builder.append(
+                        TextCensor.censorBannedWords(new String(rs.getBytes("originTweetText"), StandardCharsets.UTF_8))
+                                .replace(";", "\n"));
+                builder.append(NEW_LINE);
+            }
+        } catch (SQLException ex) {
+            logger.error("Problem executing statement", ex);
+        }
+
+        return builder.toString();
+    }
 }
---
+++ server/src/main/java/org/cloudburst/server/servlets/Q4Servlet.java
@@ -0,0 +1,60 @@
+package org.cloudburst.server.servlets;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.cloudburst.server.services.MySQLService;
+import org.cloudburst.server.util.MySQLConnectionFactory;
+
+/**
+ * Servlet for Q2.
+ */
+public class Q4Servlet extends HttpServlet {
+
+    private MySQLService mySQLService = new MySQLService(new MySQLConnectionFactory());
+
+    private static String FIRST_LINE;
+
+    public static void setFirstLine(String teamId, String teamAWSId) {
+        FIRST_LINE = teamId + "," + teamAWSId + "\n";
+    }
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        initMySqlService();
+    }
+
+    /**
+     * Initialize MySQL connection pool.
+     */
+    private void initMySqlService() {
+        Properties boneCPConfigProperties = new Properties();
+        try {
+            boneCPConfigProperties.load(Q4Servlet.class.getResourceAsStream("/bonecp.properties"));
+        } catch (IOException ex) {
+        }
+
+        MySQLConnectionFactory.init(boneCPConfigProperties);
+    }
+
+    @Override
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String hashTag = request.getParameter("hashtag");
+        int limit = Integer.parseInt(request.getParameter("n"));
+
+        String outMessage = FIRST_LINE + mySQLService.getTopHashtags(hashTag, limit);
+
+        response.setHeader("Content-Type", "text/plain; charset=UTF-8");
+        response.getOutputStream().write(outMessage.toString().getBytes());
+    }
+
+}
