be2bb69d29ef7357909a2929d71503282ef23bf0
Gaurav Walia <email.gaurav.walia@gmail.com>
2015-11-12 19:35:29 -0800

    Main.java clean up.

--- etl/src/main/java/org/cloudburst/etl/Main.java
+++ etl/src/main/java/org/cloudburst/etl/Main.java
@@ -1,82 +1,95 @@
 package org.cloudburst.etl;
 
-import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.io.IOException;
 
-import com.google.gson.*;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.cloudburst.etl.util.StringUtil;
+import org.cloudburst.etl.util.TextCensor;
 
-import org.cloudburst.etl.model.Tweet;
-import org.cloudburst.etl.util.*;
+import com.google.gson.JsonSyntaxException;
 
 /**
- * Main class to process tweets and insert them into MySQL and create an output file.
+ * Main class to process tweets and insert them into MySQL and create an output
+ * file.
  */
 public class Main {
-	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			try {
-				String line = value.toString();
+    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 
-				String[] tokens = line.split("\t");
-				StringBuilder builder = new StringBuilder();
-				String tweetId = tokens[0];
+        /**
+         * Mapper method that keeps TweetId as the KEY, and all other fields as
+         * the VALUE.
+         */
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            try {
+                String line = value.toString();
 
-				builder.append(",");
-				builder.append(tokens[3]);
-				builder.append(",");
-				builder.append(tokens[4]);
-				builder.append(",");
-				builder.append(tokens[6]);
-				builder.append(",");
-				String censored = TextCensor.censorBannedWords(new String(Hex.decodeHex(tokens[7].toCharArray())));
-				builder.append(StringUtil.bytesToHex(censored.getBytes()));
+                String[] tokens = line.split("\t");
+                StringBuilder builder = new StringBuilder();
+                String tweetId = tokens[0];
 
-				context.write(new Text(tweetId), new Text(builder.toString()));
-			} catch (JsonSyntaxException e) {}
-			catch (DecoderException e) {}
-		}
-	}
+                builder.append(",");
+                builder.append(tokens[3]);
+                builder.append(",");
+                builder.append(tokens[4]);
+                builder.append(",");
+                builder.append(tokens[6]);
+                builder.append(",");
+                String censored = TextCensor.censorBannedWords(new String(Hex.decodeHex(tokens[7].toCharArray())));
+                builder.append(StringUtil.bytesToHex(censored.getBytes()));
 
-	public static class Reduce extends Reducer<Text, Text, Text, Text> {
+                context.write(new Text(tweetId), new Text(builder.toString()));
 
-		public void reduce(Text key, Iterable<Text> values, Context context)
-				throws IOException, InterruptedException {
-			for (Text value : values) {
-				context.write(key, value);
-				break;
-			}
-		}
-	}
+            } catch (JsonSyntaxException | DecoderException e) {
+            }
+        }
+    }
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-		Configuration conf = new Configuration();
-		Job job = new Job(conf, "etl");
+    public static class Reduce extends Reducer<Text, Text, Text, Text> {
 
-		job.setJarByClass(Main.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		job.setMapperClass(Map.class);
-		job.setReducerClass(Reduce.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+        /**
+         * The Reducer method simply iterates over the values, and writes it to
+         * the context.
+         */
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text value : values) {
+                context.write(key, value);
+                break;
+            }
+        }
+    }
 
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    /**
+     * The MAIN method serves as a driver of the custom job.
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "etl");
 
-		job.waitForCompletion(true);
-	}
+        job.setJarByClass(Main.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
 }
