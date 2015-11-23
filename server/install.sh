80565d14e522beac2cd2dea2f3fa5adf594ad379
Federico Ponte <fedep3@gmail.com>
2015-11-23 14:35:47 -0800

    Optimizations

--- server/install.sh
+++ server/install.sh
@@ -1,9 +1,9 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo aptitude -y install unzip oracle-java8-installer mysql-server htop
-wget http://download.eclipse.org/jetty/stable-9/dist/jetty-distribution-9.3.5.v20151012.zip
-unzip jetty-distribution-9.3.5.v20151012.zip
-mv jetty-distribution-9.3.5.v20151012 jetty
+wget http://download.eclipse.org/jetty/stable-9/dist/jetty-distribution-9.3.6.v20151106.zip
+unzip jetty-distribution-9.3.6.v20151106.zip
+mv jetty-distribution-9.3.6.v20151106 jetty
 sudo sed -i 's/default="8080"/default="80"/g' jetty/etc/jetty-http.xml
 sudo echo "JETTY_HOME=/home/ubuntu/jetty" | sudo tee /etc/default/jetty
 sudo cp jetty/bin/jetty.sh /etc/init.d/jetty
